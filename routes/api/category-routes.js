202f6f6c700dbfcaba0c261ea613aec572a2d9b6
kimberlyamaya <kimberly.kimbell89@gmail.com>
2022-01-01 01:15:47 -0800

    defined columns in category model, was able to connect make a successful GET route in insomnia

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,6 +5,12 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
+  Category.findAll()
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+    })
   // be sure to include its associated Products
 });
 
